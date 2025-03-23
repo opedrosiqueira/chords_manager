8c21321f6e351663e412a787b70bfc3d0a841bd1
opedrosiqueira <opedrosiqueira@gmail.com>
2025-03-23 15:43:50 -0400

    feat: add optional newline support for line breaking functionality

--- break_long_lines.py
+++ break_long_lines.py
@@ -2,14 +2,15 @@ import re
 
 
 def normalizar_linhas(input_file, output_file, max_chars=80, breakline=False):
+    breakline = "\n" if breakline else ""
     with open(input_file, "r", encoding="utf-8") as infile, open(output_file, "w", encoding="utf-8") as outfile:
         for linha in infile:
             while len(linha) > max_chars:
                 quebra = list(re.finditer(r"[ .,!?;:|-]", linha[:max_chars]))  # Encontra o último separador antes do limite
                 quebra = quebra[-1].start() + 1 if quebra else max_chars  # Se não encontrar separador, quebra no limite
-                outfile.write(linha[:quebra] + "\n")  # Escreve o máximo permitido
+                outfile.write(breakline + linha[:quebra] + "\n")  # Escreve o máximo permitido
                 linha = linha[quebra:]  # Remove a parte já escrita
-            outfile.write("\n" if breakline else "" + linha)
+            outfile.write(breakline + linha)
 
 
 if __name__ == "__main__":
@@ -17,5 +18,5 @@ if __name__ == "__main__":
     output_file = input(f"output file ({input_file}LinhasCurtas.txt): ") or f"{input_file}LinhasCurtas.txt"
     max_chars = int(input("Máximo de caracteres por linha (padrão 36): ") or 36)
 
-    normalizar_linhas(input_file, output_file, max_chars)
+    normalizar_linhas(input_file, output_file, max_chars, True)
     print(f"Linhas normalizadas foram salvas em {output_file}.")
